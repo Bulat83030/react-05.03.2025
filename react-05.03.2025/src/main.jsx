e56d6e74f28a16452ee914125a56da5f33cc7c15
Bulat Safin <BulatSafin83030@gmail.com>
2025-03-17 03:30:27 -0400

    hw-3

--- react-05.03.2025/src/main.jsx
+++ react-05.03.2025/src/main.jsx
@@ -1,27 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { restaurants } from "../materials/mock";
-import { StrictMode } from "react";
+import { App } from "./component/App/app";
 
 const root = document.getElementById("root");
 
-createRoot(root).render(
-  <StrictMode>
-    {restaurants.map(({ name, menu, reviews }) => (
-      <>
-        <h2>{name}</h2>
-        <h3>Menu</h3>
-        <ul>
-          {menu.map(({ name }) => (
-            <li>{name}</li>
-          ))}
-        </ul>
-        <h3>Отзывы</h3>
-        <ul>
-          {reviews.map(({ text }) => (
-            <li>{text}</li>
-          ))}
-        </ul>
-      </>
-    ))}
-  </StrictMode>
-);
+createRoot(root).render(<App />);
